5f9200af1900563a69a20fa7f9b8f295848b24ce
Kasin Sparks <kasinsparks@gmail.com>
2022-12-03 15:51:17 -0500

    Change day numbering to match AoC

--- 2022/days/day1/main.cpp
+++ 2022/days/day1/main.cpp
@@ -2,28 +2,59 @@
 #include <sys/types.h>
 
 #include "../../modules/file_reader.hpp"
-#include "rps_player.hpp"
+#include "elf.hpp"
 
 
 int main() {
-	std::cout << "Advent of Code Day 02!" << std::endl << std::endl;
+	std::cout << "Advent of Code Day 0!" << std::endl << std::endl;
 
-	std::list<std::string> file_data;
-	//read_file(file_data, "./data/sample_input.txt");
-	read_file(file_data, "./data/input.txt");
+	//std::list<std::string> test = read_file("./data/sample_input.txt");
+	std::list<std::string> test = read_file("./data/input.txt");
+	std::list<elf*> elfs;
 	
-	uint32_t total_score = 0;
+	elf* current_elf = new elf();
 
-	for (std::string str_data : file_data) {
-		rps_player rps;
-		uint16_t score = rps.play_round(str_data);
-		std::cout << score << std::endl;
-		total_score += score;
-
-		//std::cout << str_data << std::endl;
+	for(std::string t : test) {
+		if (t.empty()) {
+			// Next elf
+			elfs.push_back(current_elf);
+			current_elf = new elf();
+		} else {
+			current_elf->add_food(std::stoi(t));
+		}
 	}
 
-	std::cout << "Total: " << total_score << std::endl;
+	// Include the last elf
+	elfs.push_back(current_elf);
 
-		
+	// Find the top three elfs
+	//elfs.sort();
+	elfs.sort([](elf* lhs, elf* rhs) {return lhs->get_total_calories() < rhs->get_total_calories();});
+	
+	for (elf* e : elfs) {
+		std::cout << e->get_total_calories() << std::endl;
+	}
+	
+	std::list<elf*>::iterator it = elfs.end();
+	it--;
+	u_int8_t count = 0;
+
+	u_int32_t calories_max = 0;
+	while (it != elfs.begin()) {
+		if (count >= 3) {
+			break;
+		}
+		std::cout << (*it)->get_total_calories() << std::endl;
+		calories_max += (*it)->get_total_calories();
+		it--;
+		count++;
+	}
+
+	std::cout << "Top Three Max Calories: " << calories_max << std::endl;
+
+
+	for (elf* e : elfs) {
+		e->~elf();
+		delete e;
+	}
 }
