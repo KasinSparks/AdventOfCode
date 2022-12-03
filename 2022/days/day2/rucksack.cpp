96e1783761ce77b895787865f9d9875cbfda10d1
Kasin Sparks <kasinsparks@gmail.com>
2022-12-03 15:38:06 -0500

    Day3 part 2

--- 2022/days/day2/rucksack.cpp
+++ 2022/days/day2/rucksack.cpp
@@ -3,43 +3,60 @@
 #include <exception>
 #include <unordered_map>
 #include <iostream>
-#include <utility>
 
-rucksack::rucksack(const std::string &line) {
-	uint32_t half_point = line.size() / 2;
-	
-	// Create a hashtable to group the two pairs of rucksacks and find the 
-	// duplicate item
-	std::unordered_map<char, uint8_t> map;
-	
-	// Add the first rucksack's contents to the hashtable
-	for (int i = 0; i < half_point; ++i) {
-		map.insert(std::pair<char, uint8_t>(line[i], 1));
-	}
-	
-
-	// Add the seconds rucksack contents
-	for (int i = half_point; i < line.size(); ++i) {
-		auto search = map.find(line[i]);
-		if (search != map.end()) {
-			//std::cout << search->first << std::endl;
-			this->_item = search->first;
-			break;
-		}
-	}
-	
+rucksack::rucksack() {
+	this->_items = std::unordered_map<char, uint8_t>();
 }
 
 uint32_t
-rucksack::get_duplicate_item_priority() {
+rucksack::get_item_priority() {
+	char item = 0;
+	// Find the item that appears 3 times
+	//std::cout << this->_items.size() << std::endl;
+	for (std::pair<char, uint8_t> m : this->_items) {
+		//std::cout << m.first << m.second << std::endl;
+		if (m.second == 3) {
+			item = m.first;
+			break;
+		}
+	}
+
+	if (item == 0) {
+		std::cout << "ERROR:: NO ITEM APPEARS 3 TIMES!" << std::endl;
+		for (std::pair<char, uint8_t> i : this->_items) {
+			std::cout << "<" << i.first << ", " << int (i.second) << ">" << std::endl;
+		}
+		throw new std::exception();
+	}
+
 	uint32_t value = 0;
-	if (this->_item >= 'A' && this->_item <= 'Z') {
-		value = (this->_item - 'A') + 27;
-	} else if (this->_item >= 'a' && this->_item <= 'z') {
-		value = (this->_item - 'a') + 1;
+	if (item >= 'A' && item <= 'Z') {
+		value = (item - 'A') + 27;
+	} else if (item >= 'a' && item <= 'z') {
+		value = (item - 'a') + 1;
 	} else {
 		throw new std::exception();
 	}
 
 	return value;
+}
+
+void
+rucksack::add_line(const std::string &line) {
+	std::unordered_map<char, uint8_t> map;
+
+	map = this->_remove_duplicates(line);
+	//std::cout << "map size: " << map.size() << std::endl;
+	
+	for (std::pair<char, uint8_t> p : map) {
+		auto search = this->_items.find(p.first);
+		if (search != this->_items.end()) {
+			// Found the item. Increase the count
+			search->second += 1;
+			//this->_items = search->first;
+		} else {
+			// Item does not exist in the hashmap, add it now!
+			this->_items.insert(std::pair<char, uint8_t>(p.first, 1));
+		}
+	}
 }
