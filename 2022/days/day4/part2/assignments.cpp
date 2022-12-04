6341015234168468dcda6f910688714e8056742c
Kasin Sparks <kasinsparks@gmail.com>
2022-12-04 00:37:16 -0500

    Day4

---
+++ 2022/days/day4/part2/assignments.cpp
@@ -0,0 +1,34 @@
+#include "assignments.hpp"
+#include <cstdint>
+
+assignment::assignment() {
+	this->_beginning = 0;
+	this->_beginning = 0;
+}
+
+assignment::assignment(const assignment &a) {
+	this->_beginning = a.get_beginning();
+	this->_ending = a.get_ending();
+}
+
+assignment::assignment(const uint32_t b, const uint32_t e) {
+	this->_beginning = b;
+	this->_ending = e;
+}
+
+uint32_t
+assignment::get_beginning() const { return this->_beginning; }
+
+uint32_t
+assignment::get_ending() const { return this->_ending; }
+
+
+bool
+assignment::fully_contains(const assignment &a) {
+	return this->_beginning <= a.get_beginning() && this->_ending >= a.get_ending();
+}
+
+bool
+assignment::partially_contains(const assignment &a) {
+	return this->_beginning <= a.get_ending() && this->_ending >= a.get_beginning();
+}
