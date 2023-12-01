965166f16c5dfffc8b861f18696f1deb1a0a3a77
Kasin Sparks <kasinsparks@gmail.com>
2023-12-01 02:20:18 -0500

    2023_Day_01

--- 2023/src/days/day_01/part1.rs
+++ 2023/src/days/day_01/part1.rs
@@ -1,9 +1,38 @@
-use crate::utils::io::{read_file};
+use crate::utils::io::{read_file_split_newlines};
 
 pub fn sln(input_path: &str) -> i32 {
-    let input: String = read_file(input_path);
+    let input: Vec<String> = read_file_split_newlines(input_path);
+    let mut result: i32 = 0;
 
-    return 0;
+    for line in input {
+        let mut first_digit: i32 = 0;
+        let mut last_digit: i32= 0;
+
+        let line_bytes = line.as_bytes();
+
+        // Find the first digit
+        for i in 0..line_bytes.len() {
+            if line_bytes[i].is_ascii_digit() {
+                first_digit = (line_bytes[i] - ('0' as u8)) as i32;
+                break;
+            }
+        }
+
+        // Find the last digit
+        for i in (0..line_bytes.len()).rev() {
+            if line_bytes[i].is_ascii_digit() {
+                last_digit = (line_bytes[i] - ('0' as u8)) as i32;
+                break;
+            }
+        }
+        
+        // Concat and convert to integer
+        result += (first_digit * 10) + last_digit;
+        println!("LINE: {}, {}, {}", line, first_digit, last_digit);
+    }
+    
+
+    return result;
 }
 
 #[cfg(test)]
@@ -12,11 +41,13 @@ mod tests {
 
     #[test]
     fn practice_result() {
-        assert_eq!(sln("./src/days/day_01/practice_input.txt"), 0);
+        assert_eq!(sln("./src/days/day_01/practice_input.txt"), 142);
     }
 
+    /*
     #[test]
     fn final_result() {
         assert_eq!(sln("./src/days/day_01/input.txt"), 0);
     }
+    */
 }
--- 2023/src/main.rs
+++ 2023/src/main.rs
@@ -6,6 +6,7 @@ use crate::days::*;
 
 fn main() {
     println!("Advent of Code");
-    let result = day_01::part1::sln("./src/days/day_01/input.txt");
+    //let result = day_01::part1::sln("./src/days/day_01/input.txt");
+    let result = day_01::part2::sln("./src/days/day_01/input.txt");
     println!("Result: {}", result);
 }
