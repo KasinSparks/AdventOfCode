1b44ec1c03ab41a83004273025ad194f7f4b91a3
Kasin Sparks <kasinsparks@gmail.com>
2023-12-02 01:48:30 -0500

    Day02: Python is def easier than rust for this...

--- 2023/src/main.rs
+++ 2023/src/main.rs
@@ -7,6 +7,8 @@ use crate::days::*;
 fn main() {
     println!("Advent of Code");
     //let result = day_01::part1::sln("./src/days/day_01/input.txt");
-    let result = day_01::part2::sln("./src/days/day_01/input.txt");
+    //let result = day_01::part2::sln("./src/days/day_01/input.txt");
+    //let result = day_02::part1::sln("./src/days/day_02/input.txt");
+    let result = day_02::part2::sln("./src/days/day_02/input.txt");
     println!("Result: {}", result);
 }
